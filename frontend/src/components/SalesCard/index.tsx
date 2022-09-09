7422e2164078d3dd445122acfe8e7393d2049ccc
Alisson Youssf <youssfbr@gmail.com>
2022-09-09 10:35:09 -0300

    Header, SalesCard components

---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,51 @@
+import NotificationButton from "../NotificationButton";
+import "./styles.css";
+
+function SalesCard() {
+  return (
+    <div className="meta-card">
+      <h2 className="meta-sales-title">Vendas</h2>
+      <div>
+        <div className="meta-form-control-container">
+          <input className="meta-form-control" type="text" />
+        </div>
+        <div className="meta-form-control-container">
+          <input className="meta-form-control" type="text" />
+        </div>
+      </div>
+
+      <div>
+        <table className="meta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="show992">1</td>
+              <td className="show576">09/09/2022</td>
+              <td>Alisson Youssf</td>
+              <td className="show992">1502</td>
+              <td className="show992">1254</td>
+              <td>R$ 155.975</td>
+              <td>
+                <div className="meta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default SalesCard;
