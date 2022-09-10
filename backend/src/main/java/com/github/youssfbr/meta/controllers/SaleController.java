85b3682dfcec50268c3c7126cd6860b3b3bfcf2b
Alisson Youssf <youssfbr@gmail.com>
2022-09-10 11:35:24 -0300

    Twilio SMS

--- backend/src/main/java/com/github/youssfbr/meta/controllers/SaleController.java
+++ backend/src/main/java/com/github/youssfbr/meta/controllers/SaleController.java
@@ -2,12 +2,10 @@ package com.github.youssfbr.meta.controllers;
 
 import com.github.youssfbr.meta.entities.Sale;
 import com.github.youssfbr.meta.services.ISaleService;
+import com.github.youssfbr.meta.services.SmsService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/sales")
@@ -15,8 +13,11 @@ public class SaleController {
 
     private final ISaleService saleService;
 
-    public SaleController(ISaleService saleService) {
+    private final SmsService smsService;
+
+    public SaleController(ISaleService saleService, SmsService smsService) {
         this.saleService = saleService;
+        this.smsService = smsService;
     }
 
     @GetMapping
@@ -25,6 +26,11 @@ public class SaleController {
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return saleService.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 
 }
