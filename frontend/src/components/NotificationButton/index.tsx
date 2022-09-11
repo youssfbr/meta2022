1dc7ddceb619d8825d0935b763c9e150eb0b20eb
Alisson Youssf <youssfbr@gmail.com>
2022-09-11 10:18:23 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,25 @@
+import axios from 'axios';
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from '../../utils/request';
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function NotificationButton( {saleId} : Props) {
   return (
-    <div className="red-btn-notification">
+    <div className="red-btn-notification" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   );
+}
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/api/v1/sales/${id}/notification`)
+    .then(response => {
+      console.log("SUCESSO");      
+    });      
 }
 
 export default NotificationButton;
